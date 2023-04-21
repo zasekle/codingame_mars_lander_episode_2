0b4cc29b4ac21ba8f800ced3e9799f6e8221e56d
Jeremiah Gleason <quickshiftx@gmail.com>
2023-04-20 22:58:10 -0400

    Still planning exactly how to do it. I have the general idea I think, but I need to narrow it down.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Point {
     x: u32,
     y: u32,
@@ -44,20 +44,43 @@ fn main() {
     //   choosing easier.
 }
 
-const DIVISOR: usize = 50;
+const DIVISOR: u32 = 50;
 
+const WIDTH: usize = (7000 / DIVISOR) as usize;
+const HEIGHT: usize = (3000 / DIVISOR) as usize;
+
+#[derive(Debug, Clone)]
 struct PathInfo {
     path: Vec<Point>,
     distance: u32,
 }
 
+#[derive(Debug, Copy, Clone)]
+struct PointPair {
+    start: Point,
+    end: Point,
+}
+
 //TODO: may want to add the starting and stopping points
 // more than one could exist tho
 // they could change directions while inside the node
+#[derive(Debug, Copy, Clone)]
 struct MapNode {
-    contains_line: bool,
+    crossing_lines: [Option<PointPair>; 5],
+    crossing_lines_idx: usize,
     contains_landing_line: bool,
     has_been_used: bool,
+}
+
+impl MapNode {
+    fn new() -> MapNode {
+        MapNode {
+            crossing_lines: [None; 5],
+            crossing_lines_idx: 0,
+            contains_landing_line: false,
+            has_been_used: false,
+        }
+    }
 }
 
 fn calculate_line(
@@ -65,109 +88,177 @@ fn calculate_line(
     flat_surface_first_index: usize,
     flat_surface_second_index: usize,
     shuttle_point: &Point,
-) {
-    //TODO: I can draw the entire thing as a grid, then draw a straight path between the starting point and each point on the line
-    // but I can't have 90 degree turns or anything like that
-    //TODO: probably something like a shortest path is more appropriate
-    // so I can divide it up into squares that are size 10(?), makes it 700x300 or 210000 points
-    // OR I can divide it up into say size 100(?), which makes it 70x30 or 2100 points
-    // this would be fairly easy to calculate and I could probably 'iron' it out if I needed to
-
-    //TODO: Will divide them into boxes of size 100
-    // any point inside of the box will need to be taken into account
-    // bring in the list of ground points, the shuttle points, the flat service indexes
-
-    //TODO: So my first thought is just to divide them all by 50, but I don't think this actually works, in fact I probably just want to use
-    // the standard coords, need to find if it intersects with anything
-    // only need to go down left and right, the shuttle will never need to go up
-
+) -> Vec<PathInfo> {
     //Breadth first search, then find the shortest path
 
-    let mut map = [[false; 7000 / DIVISOR]; 3000 / DIVISOR];
+    let mut map = [[MapNode::new(); WIDTH]; HEIGHT];
 
-    //TODO: is this the best way to do it?
-    // the alternative is to iterate through every ground point every move though
+    //Iterate through all lines and save them to their respective map nodes.
     for i in 1..ground_points.len() {
-        let start_point = &ground_points[i-1];
+        let start_point = &ground_points[i - 1];
         let end_point = &ground_points[i];
-
-        //TODO: there are straight lines and there are curved lines
-
-        //TODO: properties
-        // it seems to always 'step'
-        // maybe do intersections of straight lines and see which ones it crosses?
-
-        //TODO: maybe I can
-        // iterate through each x coordinate of a line segment
-        // intersect it with each y coordinate line
-        // use that intersection to see which y boxes were passed through during it (the intersection is the last one but there could be only one)
 
         //y1 = m * x1 + b
         //y2 = m * x2 + b
         //m = (y2-y1)/(x2-x1)
         //b = y1-m*x1
 
-        let start_x = start_point.x as f32;
-        let start_y = start_point.y as f32;
-        let end_x = end_point.x as f32;
-        let end_y = end_point.y as f32;
+        let start_point_x_f = start_point.x as f32;
+        let start_point_y_f = start_point.y as f32;
+        let end_point_x_f = end_point.x as f32;
+        let end_point_y_f = end_point.y as f32;
 
-        let m = (end_y - start_y)/(end_x - start_x);
-        let b = start_y - m * start_x;
+        let m = (end_point_y_f - start_point_y_f) / (end_point_x_f - start_point_x_f);
+        let b = start_point_y_f - m * start_point_x_f;
 
+        let start_x = start_point.x / DIVISOR;
+        let end_x = end_point.x / DIVISOR;
+
+        for x in start_x..=end_x {
+            let mut y_begin =
+                if x == start_point.x {
+                    start_point.y
+                } else {
+                    (m * (x as f32) + b) as u32
+                };
+
+            let mut y_end =
+                if x == end_point.x {
+                    end_point.y
+                } else {
+                    (m * ((x + 1) as f32) + b) as u32
+                };
+
+            y_begin /= DIVISOR;
+            y_end /= DIVISOR;
+
+            for y in y_begin..=y_end {
+                let mut map_ele = &mut map[y as usize][x as usize];
+
+                map_ele.crossing_lines[map_ele.crossing_lines_idx] =
+                    Some(
+                        PointPair {
+                            start: start_point.clone(),
+                            end: end_point.clone(),
+                        }
+                    );
+
+                map_ele.crossing_lines_idx += 1;
+
+                if i - 1 == flat_surface_first_index && i == flat_surface_second_index {
+                    map_ele.contains_landing_line = true;
+                }
+            }
+        }
     }
 
-    //TODO: the first move and last move will be difficult because they aern't at even numbers
-    // I suppose I need to make sure that the square I am in does not intersect with anything
-    // then I can just start from all points around it,
-    // maybe keep track of the distance to each corner and path as a whole
-    // then I can find the shortest path by the path total distance
-
-    //TODO: need to send in all 2 points 'around' the ship
-
     let mut first_point = Point {
-        x: shuttle_point.x / DIVISOR,
-        y: shuttle_point.y / DIVISOR,
+        x: shuttle_point.x,
+        y: shuttle_point.y,
     };
 
-    //mark the point on the map
-    map[first_point.y][first_point.x] = true;
+    let first_distance = (first_point.y - shuttle_point.y).pow(2) + (first_point.x - shuttle_point.x).pow(2);
 
-    //TODO: would conceptualizing this point in the 'center' work out better for me?
-    // it might be, but all I need to do is keep the 'boxes' the same and change where the intersection is?
-    // I think it is more overhead in terms of programming, there would be less points because the
-    // right and top will actually lose a row, actually no it won't, all points will just move up and right
+    //TODO: only add the point if it is valid
+    let mut second_point = Point {
+        x: shuttle_point.x + DIVISOR,
+        y: shuttle_point.y,
+    };
 
-    //TODO: need to make sure first_point does not intersect
-    // make sure no lines go through this box maybe? If it does will need to look 'closer'
-    // maybe make the map a struct with the starting and stopping points of each intersecting line and if the square has already been passed
+    let second_distance = (second_point.y - shuttle_point.y).pow(2) + (second_point.y - shuttle_point.x).pow(2);
+    //For now assuming first points cannot have any lines to intersect with.
 
-    //TODO: probably iterate through the 'ground' itself and check for intersections
-    let first_distance = (first_point.y * DIVISOR - shuttle_point.y).pow(2) + (first_point.x * DIVISOR - shuttle_point.x).pow(2);
+    //Mark the points on the map.
+    map[(first_point.y / DIVISOR) as usize][(first_point.x / DIVISOR) as usize].has_been_used = true;
+    map[(second_point.y / DIVISOR) as usize][(second_point.x / DIVISOR) as usize].has_been_used = true;
 
-    let mut paths = Vec::<PathInfo>::new();
-
-    paths.push(
+    let mut paths = Vec::<PathInfo>::from([
         PathInfo {
-            path: Vec::from([first_point]),
+            path: Vec::from([shuttle_point.clone(), first_point]),
             distance: first_distance,
+        },
+        PathInfo {
+            path: Vec::from([shuttle_point.clone(), second_point]),
+            distance: second_distance,
         }
-    );
-
-
-
-
-
+    ]);
 
     let mut final_paths = Vec::<PathInfo>::new();
 
+    //TODO: could probably use diagonals, but there are some issues
+    // would probably need to find the shortest path to a point
+    // ./|.
+    // .\|.
+    // for example the above two paths are not equal, its b/c the lines are different lengths
     let mut distance_modifier = 1;
     while paths.is_empty() {
         let mut paths_copy = paths;
         paths = Vec::<PathInfo>::new();
 
-        for path in paths.iter_mut() {
+        for path in paths_copy {
+            let final_x = path.path.last().expect("path empty").x;
+            let final_y = path.path.last().expect("path empty").y;
 
+            //TODO: Is 0 correct here or should it be < DIVISOR?
+            if final_x != 0 { //left
+                let mut path_ended = false;
+                let next_x = final_x - DIVISOR;
+                let next_element = &mut map[(next_x / DIVISOR) as usize][(final_y / DIVISOR) as usize];
+                for i in 0..next_element.crossing_lines_idx {
+                    let point_pair = next_element.crossing_lines[i].expect("invalid crossing idx {i}");
+
+                    let start_point_x_f = point_pair.start.x as f32;
+                    let start_point_y_f = point_pair.start.y as f32;
+                    let end_point_x_f = point_pair.end.x as f32;
+                    let end_point_y_f = point_pair.end.y as f32;
+
+                    let m = (end_point_y_f - start_point_y_f) / (end_point_x_f - start_point_x_f);
+                    let b = start_point_y_f - m * start_point_x_f;
+                    let y_line = final_y as f32;
+
+                    //line is y=(final_y*DIVISOR)
+                    //y = mx+b
+                    //x = (final_y*DIVISOR-b)/m
+
+                    let x_intersection = ((y_line - b) / m) as u32;
+
+                    if (x_intersection / DIVISOR) == (final_x / DIVISOR) {
+                        path_ended = true;
+
+                        if point_pair.start == ground_points[flat_surface_first_index]
+                            && point_pair.end == ground_points[flat_surface_second_index] {
+
+                            let mut path_clone = path.clone();
+                            path_clone.path.push(
+                                Point {
+                                    x: x_intersection,
+                                    y: final_y,
+                                }
+                            );
+
+                            //TODO: calculate distance, remember I am not using sqrt
+
+                            final_paths.push(
+                                path_clone
+                            );
+                        }
+                    }
+                }
+
+                //TODO: if no intersections, add the new path and calculate new distance
+                //TODO: mark the point on the map
+            }
+
+            //TODO: final_x has to do with 6999 but not sure its correct
+            if final_x != 6999 { //right
+            }
+
+            if final_y != 0 { //down
+            }
         }
+
+        distance_modifier += 1;
     }
+
+    final_paths
 }
